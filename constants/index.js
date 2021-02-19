13d1974cf8db9e5870732b37d4960d64dcbad9d2
Ronald Pranata <ronald.pranata@isobar.com>
2021-02-19 15:02:32 +0800

    add translation function

---
+++ constants/index.js
@@ -0,0 +1,50 @@
+export const translation ={
+  periodDescription:"",
+  signInButton:"SIGN IN <BR/><small>to win prizes</small>",
+  menu:{
+    home:"Home",
+    privacy:"Privacy",
+    terms:"Terms",
+    logout:"Logout",
+    myRewards:"My Rewards"
+  },
+  countdown:{
+    days:"DAYS",
+    hrs:"HRS",
+    mins:"MINS",
+    secs:"SECS",
+  },
+  submissionText:{
+    header:"Enter your code",
+    siginHeader:"Sign in or register a Coca-Cola <br>account to enter the draw",
+    name:"Name",
+    email:"Email",
+    phoneNumber:"Phone Number",
+    acceptTerm:'I accept the <a href="/tnc">Terms and Conditions</a> of this this redemption.',
+    acceptPrivacy:'I accept the <a href="/privacy">Privacy Policy</a> of this redemption.',
+    declareAge:'I declare that I am above 13  years old.',
+    enterCode:'Enter Unique Code',
+    tooltipText:'Enter the code found under the cap/tab of your Coca Cola purchase here',
+    buttonText:'Collect your prize',
+    errorTerm:'Please accept our terms and conditions',
+    errorPolicy:"Please accept our privacy policies",
+    errorDeclare:"Please declare that you are above 13  years old",
+    errorPinCode:"Oops your pin code is invalid or already redeemed",
+    errorAPI:"Oops something went wrong please try again",
+    errorMaxPhone:"You reach maximum phone number length",
+    participateAgain:"Participate again",
+    listenNow:"Listen now"
+  },
+  winnerText:{
+    header:"WINNER LIST"
+  },
+  globalError:{
+    errorAPI:"Oops something went wrong",
+    tryagain:"Please try again"
+  },
+  footer:{
+    privacy:"Privacy Policy",
+    terms:"Terms and conditions"
+  }
+
+}
