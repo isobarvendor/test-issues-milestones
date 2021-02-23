8779a7c849110ed79070648949e10ce214561170
Shahin Alam <shahin.alam@isobar.com>
2021-02-23 14:24:49 +0800

    Use the old version of babel preset-env

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,11 +115,5 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
-    babel: {
-      presets({ isServer }) {
-        const targets = isServer ? { node: "current" } : { ie: 11 };
-        return [[require.resolve("@babel/preset-env"), { targets }]];
-      },
-    },
   },
 };
