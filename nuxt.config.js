b07648f403fab7e324f7806784ab9e55c2574536
Shahin Alam <shahin.alam@isobar.com>
2021-02-23 14:15:46 +0800

    Beautify the nuxt config and added babel build config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,119 +1,125 @@
-
 export default {
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
 
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "server",
   /*
-  ** ENVs
-  */
+   ** ENVs
+   */
   publicRuntimeConfig: {
     //baseURL: 'https://nuxtjs.org'
-    pixelUrl:process.env.pixelUrl,
-    cdsTrackingClientId:process.env.cdsTrackingClientId,
-    phoneCode:process.env.phoneCode,
-    maxPhoneNumber:process.env.maxPhoneNumber,
-    voucherParameter:process.env.voucherParameter,
-    configID:process.env.configID,
-    lotID:process.env.lotID,
+    pixelUrl: process.env.pixelUrl,
+    cdsTrackingClientId: process.env.cdsTrackingClientId,
+    phoneCode: process.env.phoneCode,
+    maxPhoneNumber: process.env.maxPhoneNumber,
+    voucherParameter: process.env.voucherParameter,
+    configID: process.env.configID,
+    lotID: process.env.lotID,
   },
   privateRuntimeConfig: {
     //apiSecret: process.env.API_SECRET
   },
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'img/icons/favicon.png' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "img/icons/favicon.png" },
+    ],
   },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
-    '@/assets/scss/main.scss',
-    '@/assets/scss/fonts.scss',
-    'swiper/css/swiper.css'
+    "@/assets/scss/main.scss",
+    "@/assets/scss/fonts.scss",
+    "swiper/css/swiper.css",
   ],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
   plugins: [
     "./plugins/vue-awesome-swiper.js",
-    './plugins/axios', //axios interceptor
-    { src: '~/plugins/vuex-persist', ssr: false },
-    { src: '~/plugins/pixel', ssr: false },
+    "./plugins/axios", //axios interceptor
+    { src: "~/plugins/vuex-persist", ssr: false },
+    { src: "~/plugins/pixel", ssr: false },
     {
-      src: '~/plugins/vue-video-background',
-      ssr: false
+      src: "~/plugins/vue-video-background",
+      ssr: false,
     },
     {
-      src: '~/plugins/vue-countdown',
-      ssr: false
-    }
+      src: "~/plugins/vue-countdown",
+      ssr: false,
+    },
   ],
   router: {
-    middleware: ['redirect']
-},
+    middleware: ["redirect"],
+  },
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
   components: true,
   /*
-  ** Nuxt.js dev-modules
-  */
- buildModules: [
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
     // Simple usage
-    '@nuxtjs/vuetify',
-    'vue-scrollto/nuxt',
+    "@nuxtjs/vuetify",
+    "vue-scrollto/nuxt",
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/dotenv',
-    'nuxt-validate',
+    "@nuxtjs/axios",
+    "@nuxtjs/dotenv",
+    "nuxt-validate",
     [
-      'nuxt-mq',
+      "nuxt-mq",
       {
         // Default breakpoint for SSR
-        defaultBreakpoint: 'sm',
+        defaultBreakpoint: "sm",
         breakpoints: {
           sm: 576,
           md: 768,
           lg: 992,
-          xl: Infinity
-        }
-      }
-    ]
+          xl: Infinity,
+        },
+      },
+    ],
   ],
   styleResources: {
-    scss: [
-      '~assets/scss/variables.scss',
-      '~assets/scss/mixins.scss',
-    ]
+    scss: ["~assets/scss/variables.scss", "~assets/scss/mixins.scss"],
   },
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
   build: {
-  }
-}
+    babel: {
+      presets({ isServer }) {
+        const targets = isServer ? { node: "current" } : { ie: 11 };
+        return [[require.resolve("@babel/preset-env"), { targets }]];
+      },
+    },
+  },
+};
