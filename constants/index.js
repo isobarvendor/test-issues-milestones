6d8749ddce17d7c177154e8198494f8804676d26
Ronald Pranata <ronald.pranata@isobar.com>
2021-03-23 17:52:20 +0800

    change language

--- constants/index.js
+++ constants/index.js
@@ -74,74 +74,79 @@ let language =
     }
 
   },
-  th:{
-      browserTitle:"Turn Up Your Rhythm",
-      meta: [
-        {
-          hid: "Turn Up Your Rhythm",
-          name: "Turn Up Your Rhythm",
-          content: "Home page Coke - Turn Up Your Rhythm"
-        }
-      ],
-      periodDescription:"ระยะเวลาร่วมกิจกรรม 1 มีนาคม – 31 พฤษภาคม 2564 เพียงแค่ซื้อผลิตภัณฑ์โค้กที่ร่วมรายการแล้วนำโค้ดใต้ฝาสีทองมากรอก ก็รับไปเลยทันที JOOX VIP 1 สัปดาห์ (สิทธิ์มีจำนวนจำกัด 1 คน ต่อ 1 สิทธิ์) และลุ้นรับรางวัลสุดซ่าอื่นๆจากการจับรางวัลทุกสัปดาห์ เพียงแค่ทำตามขั้นตอนด้านล่าง",
-      signInButton:"SIGN IN <BR/><small>to win prizes</small>",
-      menu:{
-        home:"หน้าหลัก",
-        privacy:"นโยบายข้อมูลส่วนบุคคล",
-        terms:"งื่อนไขการเข้าร่วมกิจกรรม",
-        logout:"ออกจากระบบ",
-        myRewards:"รางวัลของฉัน"
-      },
-      countdown:{
-        days:"DAYS",
-        hrs:"HRS",
-        mins:"MINS",
-        secs:"SECS",
-      },
-      submissionText:{
-        header:"Enter your code",
-        hello:"Hello",
-        siginHeader:'ลงทะเบียนกับ "โค้ก" เพื่อร่วมกิจกรรม เปิดจังหวะซ่าในตัวคุณ',
-        name:"ชื่อ นามสกุล",
-        email:"อีเมลล์",
-        phoneNumber:"เบอร์โทรศัพท์",
-        acceptTerm:'ข้าพเจ้ายอมรับ<a href="/tnc" target="_blank">ข้อกำหนดและเงื่อนไข</a>ของกิจกรรมส่งเสริมการขายนี้​',
-        acceptPrivacy:'ข้าพเจ้ารับทราบ<a href="/privacy" target="_blank">นโยบายความเป็นส่วนตัว</a> และยินยอมให้ใช้ข้อมูลส่วนบุคคลของข้าพเจ้าที่ได้ลงทะเบียนในการเข้าร่วมกิจกรรมส่งเสริมการขายนี้​',
-        declareAge:'ข้าพเจ้ายินยอมให้ใช้ข้อมูลส่วนบุคคลของข้าพเจ้าเพื่อวัตถุประสงค์เกี่ยวกับการรับข้อมูลข่าวสารการตลาดและการโฆษณาประชาสัมพันธ์จากโคคา-โคล่า​',
-        enterCode:'ใส่รหัส 9 หลักใต้ฝาโค้ก',
-        tooltipText:'ใส่รหัส 9 หลักใต้ฝาโค้ก ที่นี่',
-        phoneTooltip:"โปรดใส่เบอร์มือถือ 9 หลักหลัง +66 โดยไม่ต้องใส่เลข 0 เช่น ถ้าเบอร์ของคุณคือ 081-123-4567 โปรดใส่ +66811234567",
-        buttonText:'กดส่งรหัสใต้ฝา',
-        errorTerm:'Please accept our terms and conditions',
-        errorPolicy:"Please accept our privacy policies",
-        errorDeclare:"Please declare that you are above 13  years old",
-        errorPinCode:"ขออภัยครับ รหัสของคุณไม่ถูกต้อง โปรดใส่รหัส 9 หลักใต้ฝาโค้กใหม่อีกครั้ง หรือไปที่หน้า รางวัลของฉัน เพื่อดูรางวัลที่คุณได้รับ",
-        errorPinCode1:"ขออภัยครับ รหัสของคุณไม่ถูกต้อง โปรดใส่รหัส 9 หลักใต้ฝาโค้กใหม่อีกครั้ง หรือไปที่หน้า รางวัลของฉัน เพื่อดูรางวัลที่คุณได้รับ",
-        errorPinCode2:"ขออภัยครับ รหัสของคุณไม่ถูกต้อง โปรดใส่รหัส 9 หลักใต้ฝาโค้กใหม่อีกครั้ง หรือไปที่หน้า รางวัลของฉัน เพื่อดูรางวัลที่คุณได้รับ",
-        errorPinCode3:"ขออภัยครับ รหัสของคุณไม่ถูกต้อง โปรดใส่รหัส 9 หลักใต้ฝาโค้กใหม่อีกครั้ง หรือไปที่หน้า รางวัลของฉัน เพื่อดูรางวัลที่คุณได้รับ",
-        errorPinCode4:"ขออภัยครับ รหัสของคุณไม่ถูกต้อง โปรดใส่รหัส 9 หลักใต้ฝาโค้กใหม่อีกครั้ง หรือไปที่หน้า รางวัลของฉัน เพื่อดูรางวัลที่คุณได้รับ",
-        errorAPI:"Oops something went wrong please try again",
-        errorMaxPhone:"You reach maximum phone number length",
-        errorRequiredPhone:"โปรดใส่เบอร์โทรศัพท์",
-        participateAgain:"ส่งรหัสใต้ฝาเพื่อลุ้นรางวัลรายสัปดาห์",
-        listenNow:"Listen now",
-        redeemPrize:"กดเพื่อรับรางวัลของคุณทันที",
-        continue:"ล็อกอินผ่าน"
-      },
-      winnerText:{
-        header:"WINNER LIST",
-        nowinner:"Coming soon"
-      },
-      globalError:{
-        errorAPI:"Oops something went wrong",
-        tryagain:"Please try again"
-      },
-      footer:{
-        privacy:"Privacy Policy",
-        terms:"Terms and conditions"
+  my:{
+    browserTitle:"Hidupkan Rentakmu",
+    meta: [
+      {
+        hid: "Hidupkan Rentakmu",
+        name: "Hidupkan Rentakmu",
+        content: "Home page Coke - Turn Up Your Rhythm"
       }
-
+    ],
+    periodDescription:"",
+    signInButton:"SIGN IN <BR/><small>to win prizes</small>",
+    menu:{
+      home:"Home",
+      privacy:"Privacy",
+      terms:"Terms",
+      logout:"Logout",
+      myRewards:"My Rewards"
+    },
+    countdown:{
+      days:"DAYS",
+      hrs:"HRS",
+      mins:"MINS",
+      secs:"SECS",
+    },
+    submissionText:{
+      header:"Enter your code",
+      hello:"Hello",
+      siginHeader:"Sign in or register a Coca-Cola <br>account to enter the draw",
+      name:"Name",
+      email:"Email",
+      phoneNumber:"Phone Number",
+      acceptTerm:'Saya berumur 18 tahun dan ke atas. Saya bersetuju dengan <a href="/tnc">terma dan syarat promosi</a>, serta menerima <a href="/privacy">dasar polisi</a>.',
+      acceptPrivacy:'I agree to the collection, use, disclosure, or processing of my personal data in accordance of <a href="/privacy">Privacy Policy</a>.',
+      declareAge:'Saya berumur bawah 18 tahun. Saya mempunyai kebenaran ibu bapa/ penjaga untuk menyertai peraduan ini yang tertakluk kepada <a href="/tnc">terma dan syarat</a>, serta menerima <a href="/privacy">dasar polisi</a>.',
+      enterCode:'Kod unik',
+      tooltipText:'Enter the code found under the cap/tab of your Coca Cola purchase here',
+      phoneTooltip:"Please input the correct phone number so that Coca-Cola can contact you directly incase you are the winner",
+      buttonText:'Klik untuk seterusnya',
+      errorTerm:'Please accept our terms and conditions',
+      errorPolicy:"Please accept our privacy policies",
+      errorDeclare:"Please declare your age and you are agree with terms and condition",
+      errorPinCode:"Oops your pin code is invalid or already redeemed",
+      errorPinCode1:"Oops your pin code is invalid or already redeemed",
+      errorPinCode2:"Oops your pin code is invalid or already redeemed",
+      errorPinCode3:"Oops your pin code is invalid or already redeemed",
+      errorPinCode4:"Oops your pin code is invalid or already redeemed",
+      errorAPI:"Oops something went wrong please try again",
+      errorMaxPhone:"You reach maximum phone number length",
+      errorRequiredPhone:"The phone number is required",
+      participateAgain:"Participate again",
+      listenNow:"Listen now",
+      redeemPrize:"Tebus Sekarang",
+      continue:"Continue with",
+      nextQuestion:"Next question",
+      redeemAnswer:"Redeem Answer",
+      submit:"Submit",
+      nextPrize:"Next Prize",
+      luckyDrawSuccess:"YOU HAVE SUCCESSFULLY ENROLLED IN THE WEEKLY LUCKY DRAW!"
+    },
+    winnerText:{
+      header:"WINNER LIST",
+      nowinner:"Coming soon"
+    },
+    globalError:{
+      errorAPI:"Oops something went wrong",
+      tryagain:"Please try again"
+    },
+    footer:{
+      privacy:"Privacy Policy",
+      terms:"Terms and conditions"
     }
+
+  }
 }
 
 
