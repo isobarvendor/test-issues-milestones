e838a8be87aeca6f8325d8cfe587d6a991c07345
farizakbar.shalahhuddin <farizakbar.shalahhuddin@isobar.com>
2021-08-16 13:55:16 +0700

    adding new translation

--- constants/index.js
+++ constants/index.js
@@ -78,84 +78,161 @@ let language =
     }
 
   },
-  vn:{
-      browserTitle:"Turn Up Your Rhythm",
-      meta: [
-        {
-          hid: "Turn Up Your Rhythm",
-          name: "Turn Up Your Rhythm",
-          content: "Home page Coke - Turn Up Your Rhythm"
-        }
-      ],
-      periodDescription:"Rất tiếc thời gian CTKM Bừng Giai Điệu Hứng Khởi đã kết thúc vào ngày 06/06/2021",
-      signInButton:"ĐĂNG NHẬP <BR/><small>để trúng thưởng</small>",
-      menu:{
-        home:"Trang Chủ",
-        privacy:"Điều khoản bảo mật",
-        terms:"Điều kiện & Điều khoản áp dụng",
-        logout:"đăng xuất",
-        myRewards:"Giải thưởng của bạn"
-      },
-      countdown:{
-        days:"DAYS",
-        hrs:"HRS",
-        mins:"MINS",
-        secs:"SECS",
-      },
-      submissionText:{
-        header:"",
-        hello:"Hello",
-        siginHeader:'Đăng kí và bắt đầu tham gia chương trình rút thăm trúng thưởng',
-        name:"Họ & Tên",
-        email:"Email",
-        phoneNumber:"Số điện thoại",
-        acceptTerm:'Tôi đồng ý với <a href="/tnc" target="_blank">Điều kiện và Điều khoản được áp dụng</a> cho chương trình khuyến mãi',
-        acceptPrivacy:'Tôi đồng ý với <a href="/privacy" target="_blank">Điều khoản Bảo mật</a> của chương trình khuyến mãi',
-        declareAge:'Tôi cam đoan đã đủ hoặc trên 18 tuổi',
-        enterCode:'Nhập mã ngay',
-        tooltipText:'Nhập mã dự thưởng bên dưới nắp khoen hay nắp chai của sản phẩm Coca-Cola có khuyến mãi',
-        phoneTooltip:"Vui lòng nhập chính xác số điện thoại để BTC có thể liên lạc trong trường hợp bạn là người trúng giải",
-        buttonText:'Nhận quà thưởng',
-        errorTerm:'Vui lòng chấp nhận các điều khoản và điều kiện của chúng tôi',
-        errorPolicy:"Vui lòng chấp nhận chính sách bảo mật của chúng tôi",
-        errorDeclare:"Vui lòng tuyên bố rằng bạn trên 18 tuổi",
-        errorPinCode:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
-        errorPinCode1:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
-        errorPinCode2:"Xin lỗi Mã Tham Dự này đã được sử dụng. Vui lòng kiểm tra lại.",
-        errorPinCode3:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
-        errorPinCode4:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
-        errorZaloPrize:"Redemption Limit for zalo prize has been reached",
-        errorNormalPrize:"Redemption Limit for normal prize has been reached",
-        errorAPI:"Rất tiếc. Đã xảy ra lỗi. Vui lòng thử lại",
-        errorMaxPhone:"Bạn đạt đến độ dài số điện thoại tối đa",
-        participateAgain:"Participate again",
-        listenNow:"Nghe nhạc ngay",
-        redeemPrize:"ĐỔI GIẢI THƯỞNG TẠI ĐÂY",
-        continue:"Tiếp tục với",
-        note:"Vui lòng gọi hotline 1900555584 (1500VND/ phút từ 9:00 đến 17:00, ngày hành chính) để nhận hướng dẫn thủ tục nhận giải thưởng.",
-        startRedeeming:"Bắt đầu rút thăm",
-        redeemNextPrize:"Tiếp tục đổi giải",
-        errorRequiredPhone:"The phone number is required",
-        textPage:"Bạn vừa nhập Mã dự thưởng từ sản phẩm chai nhựa Coca-Cola 300ml hoặc Lon Coca-Cola 235ml. Bạn có 2 cơ hội rút thăm trúng thưởng",
-        hardLuckHeader:"",
-        hardLuckTitle:"Lượt nghe một bài nhạc độc quyền của Coca-Cola",
-        hardLuckImage:"https://d1tnwdlrciourl.cloudfront.net/prizeImages/1e0b8kkwap0rj/kkwbxo87/ExclusiveSong.png",
-        hardLuckMusic:"https://elasticbeanstalk-prod-appcontent-016176.s3-ap-southeast-1.amazonaws.com/can_mong_tay_master_11-4_fix.mp3",
-      },
-      winnerText:{
-        header:"WINNER LIST",
-        nowinner:"Coming soon"
-      },
-      globalError:{
-        errorAPI:"Oops something went wrong",
-        tryagain:"Please try again"
-      },
-      footer:{
-        privacy:"Điều khoản bảo mật",
-        terms:"Điều kiện & Điều khoản áp dụng"
+  kh:{
+    browserTitle:"បញ្ចេញភាពរស់រវើករបស់អ្នក",
+    meta: [
+      {
+        hid: "បញ្ចេញភាពរស់រវើករបស់អ្នក",
+        name: "បញ្ចេញភាពរស់រវើករបស់អ្នក",
+        content: "Home page Coke - បញ្ចេញភាពរស់រវើករបស់អ្នក"
       }
-
+    ],
+    periodDescription:"កាលបរិច្ឆេទកម្មវិធី ថ្ងៃទី 04 កញ្ញា 2021 ដល់ថ្ងៃទី 30 តុលា 2021",
+    signInButton:"SIGN IN <BR/><small>to win prizes</small>",
+    menu:{
+      home:"Home",
+      privacy:"Privacy",
+      terms:"Terms",
+      logout:"Logout",
+      myRewards:"My Rewards"
+    },
+    countdown:{
+      days:"DAYS",
+      hrs:"HRS",
+      mins:"MINS",
+      secs:"SECS",
+    },
+    submissionText:{
+      header:"វាយបញ្ចូលលេខកូដ",
+      hello:"Hello",
+      siginHeader:"Sign in or register a Coca-Cola <br>account to enter the draw",
+      name:"ឈ្មោះ",
+      email:"អុីម៉េល",
+      phoneNumber:"លេខទូរស័ព្ទ",
+      acceptTerm:'ខ្ញុំយល់ព្រមទទួលយកនូវលក្ខន្ដិកនេះ',
+      acceptPrivacy:'ខ្ញុំយល់ព្រមទទួលយកនូវគោលការណ៍ឯកជនភាពនេះ',
+      declareAge:'ខ្ញុំសូមប្រកាសថាខ្ញុំមានអាយុលើស 18 ឆ្នាំ',
+      enterCode:'វាយបញ្ចូលលេខកូដ',
+      tooltipText:'វាយបញ្ចូលលេខកូដដែលឃើញពីក្រោមគម្របដប ឬក្រវិលកំប៉ុងកូកា-កូឡារបស់អ្នកនៅទីនេះ',
+      phoneTooltip:"Please input the correct phone number so that Coca-Cola can contact you directly incase you are the winner",
+      buttonText:'ទទួលយករង្វាន់របស់អ្នក',
+      errorTerm:'Please accept our terms and conditions',
+      errorPolicy:"Please accept our privacy policies",
+      errorDeclare:"Please declare that you are above 13  years old",
+      errorPinCode:"Oops your pin code is invalid or already redeemed",
+      errorPinCode1:"Oops your pin code is invalid or already redeemed",
+      errorPinCode2:"Oops your pin code is invalid or already redeemed",
+      errorPinCode3:"Oops your pin code is invalid or already redeemed",
+      errorPinCode4:"Oops your pin code is invalid or already redeemed",
+      errorZaloPrize:"Redemption Limit for zalo prize has been reached",
+      errorNormalPrize:"Redemption Limit for normal prize has been reached",
+      errorAPI:"Oops something went wrong please try again",
+      errorMaxPhone:"You reach maximum phone number length",
+      errorRequiredPhone:"The phone number is required",
+      participateAgain:"Participate again",
+      listenNow:"ស្តាប់ឥឡូវនេះ",
+      redeemPrize:"Redeem Prize",
+      continue:"Continue with",
+      note:"សូមទាក់ទងលេខ 1-800-20-2222 ដើម្បីដឹងពីរបៀបប្តូរយករង្វាន់",
+      startRedeeming:"ចាប់ផ្តើមប្តូរយករង្វាន់",
+      redeemNextPrize:"ប្តូរយករង្វាន់បន្ទាប់",
+      textPage:"អ្នកបានបញ្ចូលលេខកូដពីដបកូកា-កូឡាដបជ័រ 350ml អ្នកនឹងមានឱកាសចាប់រង្វាន់ 2 ដង",
+      hardLuckHeader:"",
+      hardLuckTitle:"",
+      hardLuckImage:""
+    },
+    winnerText:{
+      header:"WINNER LIST",
+      nowinner:"Coming soon"
+    },
+    globalError:{
+      errorAPI:"Oops something went wrong",
+      tryagain:"Please try again"
+    },
+    footer:{
+      privacy:"គោលការណ៍ឯកជន",
+      terms:"លក្ខន្ដិកនៃការចូលរួម"
     }
+
+  },
+  // vn:{
+  //     browserTitle:"Turn Up Your Rhythm",
+  //     meta: [
+  //       {
+  //         hid: "Turn Up Your Rhythm",
+  //         name: "Turn Up Your Rhythm",
+  //         content: "Home page Coke - Turn Up Your Rhythm"
+  //       }
+  //     ],
+  //     periodDescription:"Rất tiếc thời gian CTKM Bừng Giai Điệu Hứng Khởi đã kết thúc vào ngày 06/06/2021",
+  //     signInButton:"ĐĂNG NHẬP <BR/><small>để trúng thưởng</small>",
+  //     menu:{
+  //       home:"Trang Chủ",
+  //       privacy:"Điều khoản bảo mật",
+  //       terms:"Điều kiện & Điều khoản áp dụng",
+  //       logout:"đăng xuất",
+  //       myRewards:"Giải thưởng của bạn"
+  //     },
+  //     countdown:{
+  //       days:"DAYS",
+  //       hrs:"HRS",
+  //       mins:"MINS",
+  //       secs:"SECS",
+  //     },
+  //     submissionText:{
+  //       header:"",
+  //       hello:"Hello",
+  //       siginHeader:'Đăng kí và bắt đầu tham gia chương trình rút thăm trúng thưởng',
+  //       name:"Họ & Tên",
+  //       email:"Email",
+  //       phoneNumber:"Số điện thoại",
+  //       acceptTerm:'Tôi đồng ý với <a href="/tnc" target="_blank">Điều kiện và Điều khoản được áp dụng</a> cho chương trình khuyến mãi',
+  //       acceptPrivacy:'Tôi đồng ý với <a href="/privacy" target="_blank">Điều khoản Bảo mật</a> của chương trình khuyến mãi',
+  //       declareAge:'Tôi cam đoan đã đủ hoặc trên 18 tuổi',
+  //       enterCode:'Nhập mã ngay',
+  //       tooltipText:'Nhập mã dự thưởng bên dưới nắp khoen hay nắp chai của sản phẩm Coca-Cola có khuyến mãi',
+  //       phoneTooltip:"Vui lòng nhập chính xác số điện thoại để BTC có thể liên lạc trong trường hợp bạn là người trúng giải",
+  //       buttonText:'Nhận quà thưởng',
+  //       errorTerm:'Vui lòng chấp nhận các điều khoản và điều kiện của chúng tôi',
+  //       errorPolicy:"Vui lòng chấp nhận chính sách bảo mật của chúng tôi",
+  //       errorDeclare:"Vui lòng tuyên bố rằng bạn trên 18 tuổi",
+  //       errorPinCode:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
+  //       errorPinCode1:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
+  //       errorPinCode2:"Xin lỗi Mã Tham Dự này đã được sử dụng. Vui lòng kiểm tra lại.",
+  //       errorPinCode3:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
+  //       errorPinCode4:"Xin lỗi Mã Tham Dự này không đúng. Vui lòng kiểm tra lại.",
+  //       errorZaloPrize:"Redemption Limit for zalo prize has been reached",
+  //       errorNormalPrize:"Redemption Limit for normal prize has been reached",
+  //       errorAPI:"Rất tiếc. Đã xảy ra lỗi. Vui lòng thử lại",
+  //       errorMaxPhone:"Bạn đạt đến độ dài số điện thoại tối đa",
+  //       participateAgain:"Participate again",
+  //       listenNow:"Nghe nhạc ngay",
+  //       redeemPrize:"ĐỔI GIẢI THƯỞNG TẠI ĐÂY",
+  //       continue:"Tiếp tục với",
+  //       note:"Vui lòng gọi hotline 1900555584 (1500VND/ phút từ 9:00 đến 17:00, ngày hành chính) để nhận hướng dẫn thủ tục nhận giải thưởng.",
+  //       startRedeeming:"Bắt đầu rút thăm",
+  //       redeemNextPrize:"Tiếp tục đổi giải",
+  //       errorRequiredPhone:"The phone number is required",
+  //       textPage:"Bạn vừa nhập Mã dự thưởng từ sản phẩm chai nhựa Coca-Cola 300ml hoặc Lon Coca-Cola 235ml. Bạn có 2 cơ hội rút thăm trúng thưởng",
+  //       hardLuckHeader:"",
+  //       hardLuckTitle:"Lượt nghe một bài nhạc độc quyền của Coca-Cola",
+  //       hardLuckImage:"https://d1tnwdlrciourl.cloudfront.net/prizeImages/1e0b8kkwap0rj/kkwbxo87/ExclusiveSong.png",
+  //       hardLuckMusic:"https://elasticbeanstalk-prod-appcontent-016176.s3-ap-southeast-1.amazonaws.com/can_mong_tay_master_11-4_fix.mp3",
+  //     },
+  //     winnerText:{
+  //       header:"WINNER LIST",
+  //       nowinner:"Coming soon"
+  //     },
+  //     globalError:{
+  //       errorAPI:"Oops something went wrong",
+  //       tryagain:"Please try again"
+  //     },
+  //     footer:{
+  //       privacy:"Điều khoản bảo mật",
+  //       terms:"Điều kiện & Điều khoản áp dụng"
+  //     }
+
+  //   }
 }
 
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -129,5 +129,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
+    
+  
   },
 };
