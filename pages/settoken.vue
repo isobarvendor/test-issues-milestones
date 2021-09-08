c40fa50cf42ceffbe29659f6740868d9da6ac737
Ronald Pranata <ronald.pranata@isobar.com>
2021-09-08 10:43:14 +0800

    set token enhancement

--- pages/settoken.vue
+++ pages/settoken.vue
@@ -6,19 +6,27 @@
 
 export default {
 
-  mounted() {
-    this.setToken();
+  async mounted() {
+    await this.setToken();
   },
   methods: {
     async setToken() {
     if(this.$route.query.error){
       await this.$store.commit('SET_TOKEN',null)
       await this.$store.commit('SET_ERROR',this.$route.query.error)
+        if(this.$store.state.errorLogin)
+      {
+         this.$router.push("/")
+      }
     }else{
       await this.$store.commit('SET_ERROR',null)
       await this.$store.commit('SET_TOKEN',this.$route.query.token)
+      if(this.$store.state.token)
+      {
+         this.$router.push("/")
+      }
     }
-        this.$router.push("/")
+
     },
   },
 };
