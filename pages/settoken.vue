149cf7dd086625135560811f58e6e5347b267d06
Ronald Pranata <ronald.pranata@isobar.com>
2021-09-17 16:08:17 +0800

    remove old token

--- pages/settoken.vue
+++ pages/settoken.vue
@@ -19,14 +19,16 @@ export default {
          this.$router.push("/")
       }
     }else{
+      await this.$store.commit('SET_TOKEN',null)
       await this.$store.commit('SET_ERROR',null)
       await this.$store.dispatch(GET_TOKEN)
             .then((response)=>{
+                 if(this.$store.state.token){
+                      this.$router.push("/")
+                  }
             })
        // console.log("token",this.$store.state.token);
-       if(this.$store.state.token){
-          this.$router.push("/")
-       }
+
     }
 
     },
